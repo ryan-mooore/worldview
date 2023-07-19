6d0bdb3aa03ccab638f778ae598caee531867dd1
Ryan Moore <kiwi.ryanmoore@gmail.com>
2023-07-19 12:36:46 +0200

    Add support for downsampling to normalize spatial resolution

--- worldview.R
+++ worldview.R
@@ -1,6 +1,7 @@
 ZOOM <- 10
 TILESERVER <- "https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/${z}/${y}/${x}"
-SIZE <- list(x=6, y=6)
+SIZE <- c(X=4, Y=4)
+DOWNSAMPLE <- 1
 COUNTRY <- "New Zealnd"
 
 # main libraries
@@ -16,34 +17,34 @@ library(stringr)
 library(tidygeocoder)
 library(rnaturalearth)
 
-if (COUNTRY %in% rnaturalearth::ne_countries()@data$name_long) {
-  print(paste("Getting random location in", COUNTRY))
-  land <- rnaturalearth::ne_countries(
-    country=COUNTRY,
-    type="countries",
-    returnclass = "sf") |>
-    st_geometry()
-} else {
-  print("Getting random location in the World")
-  ne_options <- list(
-    scale=110,
-    type="land",
-    destdir=tempdir(),
-    category = "physical",
-    returnclass = "sf"
-  )
-  land <- tryCatch(
-    do.call(rnaturalearth::ne_load, ne_options),
-    error=function() do.call(rnaturalearth::ne_download, ne_options)
-  )
-}
-
-point <- st_sample(land |> st_make_valid(), size=1)
-coords <- point |> st_coordinates()
+# if (COUNTRY %in% rnaturalearth::ne_countries()@data$name_long) {
+#   print(paste("Getting random location in", COUNTRY))
+#   land <- rnaturalearth::ne_countries(
+#     country=COUNTRY,
+#     type="countries",
+#     returnclass = "sf"
+#   ) |> st_geometry()
+# } else {
+#   print("Getting random location in the World")
+#   ne_options <- list(
+#     scale=110,
+#     type="land",
+#     destdir=tempdir(),
+#     category = "physical",
+#     returnclass = "sf"
+#   )
+#   land <- tryCatch(
+#     do.call(rnaturalearth::ne_load, ne_options),
+#     error=function(e) do.call(rnaturalearth::ne_download, ne_options)
+#   )
+# }
+# 
+# point <- st_sample(land |> st_make_valid(), size=1)
+# coords <- point |> st_coordinates()
 origin <- slippymath::lonlat_to_tilenum(
   lon=coords[,"X"],
   lat=coords[,"Y"],
-  zoom=ZOOM
+  zoom=ZOOM + DOWNSAMPLE
 )
 
 location <- tidygeocoder::reverse_geo(
@@ -56,20 +57,21 @@ location <- tidygeocoder::reverse_geo(
 print("Downloading reference tile")
 tile_ext <- stringr::str_interp(
   TILESERVER, 
-  list(x=origin$x, y=origin$y, z=ZOOM)
+  list(x=origin$x, y=origin$y, z=ZOOM + DOWNSAMPLE)
   ) |> rast() |> ext()
 
 # create coordinate reference matrices
-x <- rep(origin$x + 1:SIZE$x, SIZE$y) |>
-  matrix(nrow=SIZE$y, ncol=SIZE$x, byrow=T)
-y <- rep(origin$y + 1:SIZE$y, SIZE$x) |>
-  matrix(nrow=SIZE$y, ncol=SIZE$x)
+SIZE <- 2 ^ (SIZE + DOWNSAMPLE - 1)
+x <- rep(origin$x + 1:SIZE["X"], SIZE["Y"]) |>
+  matrix(nrow=SIZE["Y"], ncol=SIZE["X"], byrow=T)
+y <- rep(origin$y + 1:SIZE["Y"], SIZE["X"]) |>
+  matrix(nrow=SIZE["Y"], ncol=SIZE["X"])
 relx <- (x - origin$x - 1) * tile_ext$xmax
 rely <- (origin$y - y - 1) * tile_ext$ymax # fixes y inversion
 
 print("Downloading image tiles")
 tiles <- mapply(function(x, y) {
-  tile <- str_interp(TILESERVER, list(x=x, y=y, z=ZOOM)) |> rast()
+  tile <- str_interp(TILESERVER, list(x=x, y=y, z=ZOOM + DOWNSAMPLE)) |> rast()
   tilecoords <- matrix(ncol = 2, byrow = TRUE, c(y - origin$y, x - origin$x))
   ext(tile) <- c(
       xmin=relx[tilecoords],
@@ -77,7 +79,9 @@ tiles <- mapply(function(x, y) {
       ymin=rely[tilecoords],
       ymax=(rely[tilecoords] + tile_ext$ymax)
     )
-  tile
+  raster <- tile |> aggregate(fact=2 ^ DOWNSAMPLE)
+  RGB(raster) <- c(1, 2, 3)
+  raster
   }, x, y)
 
 print("Merging image tiles")
